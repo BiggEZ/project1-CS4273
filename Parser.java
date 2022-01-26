584e4e460dffbac255de3d4780c6fe34bc1fbe9f
jallendev <developer.joseph.allen@gmail.com>
2022-01-26 10:11:31 -0600

    Test 1 of CSV Parser

---
+++ Parser.java
@@ -0,0 +1,57 @@
+import java.io.*;
+
+public class Parser{
+    private static String[] en = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    private static String[] es = {"a", "b", "c", "ch", "d", "e", "f", "g", "h", "i", "j", "k", "l", "ll", "m", "n", "\u00F1", "o", "p", "q", "r", "rr", "s", "t", "u", "v", "w", "x", "y", "z"};
+    public static void main(String[] args){
+        
+        readFile("en");
+        readFile("es");
+    }
+    
+    public static boolean readFile(String mode){
+        BufferedReader reader = null;
+        Boolean test = false;
+        
+        try {
+            String line = "";
+            reader =  new BufferedReader(new FileReader(mode + ".csv"));
+            reader.readLine();
+            
+            while ((line = reader.readLine()) != null) {
+                String[] alphabet = line.split(",");
+            }
+            
+            test = validate(mode, alphabet);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        return test;
+    }
+    
+    public static boolean validate(String mode, String[] input){
+        String[] test;
+        
+        switch (mode){
+            case "en":
+                test = en;
+                break;
+            case "es":
+                test = es;
+                break;
+            default:
+                return false;
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (!input[i].equals(test[i])) {
+                return false;
+            }
+        }
+ 
+        return true;
+    }
+}
